56292d78a9af4694119867cd002fe8b9fd80d0e5
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2020-11-26 12:18:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+//função para cotar o plano
 function cotar(){
     document.getElementById('resultado').style.display = 'block';
     document.getElementById('corporcotador').style.display = 'none';
 
     //Verificando plano selecionado
     if(document.getElementById('selecionarplanos').value === 'HapvidaAmbulatorial'){
-        hapvidaambulatorial();
-    }
-    
-        
+        cotarhapvidaambulatorial();
+    } 
 }
