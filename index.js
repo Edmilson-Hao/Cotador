8c1ce94f9be286b40c79809ce7070a4c59ade1e2
Edmilson Gonçalves <edmilsonwp@gmail.com>
2020-11-26 09:34:11 -0300

    tentando concertar soma de pessoas e valores

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+//função para cotar o plano
 function cotar(){
     document.getElementById('resultado').style.display = 'block';
     document.getElementById('corporcotador').style.display = 'none';
 
     //Verificando plano selecionado
     if(document.getElementById('selecionarplanos').value === 'HapvidaAmbulatorial'){
-        hapvidaambulatorial();
+        cotarhapvidaambulatorial();
     }
     
         
+}
+
+//Ao terminar consulta da cotação, clicar no botão voltarbtn recarrega tela anterior
+function hideresultadodiv(){
+    document.getElementById('resultado').style.display = 'none';
+    document.getElementById('corporcotador').style.display = 'block';
 }
