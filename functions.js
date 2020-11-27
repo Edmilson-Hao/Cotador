fbe4bd4eb6b7e2592d291606cf9c1d16da0533f0
edmilson gonçalves <edmilsonwp@gmail.com>
2020-11-27 16:07:30 -0300

    Cotaçao funcionando. Falta completar todas faixas etarias.

--- functions.js
+++ functions.js
@@ -1,113 +1,56 @@
 function cotarhapvidaambulatorial(){
     var totalPessoas = 0;
-    var valorTotal = null;
+    var totalValor = 0;
 
     if(document.getElementById('018').value > 0){
-        let resultado = (document.getElementById('018').value * 103.52).toFixed(2);
-        document.getElementById('resultado018').innerHTML = '0 - 18: \t' + document.getElementById('018').value + '\tR$' +  resultado;
-        totalPessoas = totalPessoas + parseFloat(document.getElementById('018').value);
-        valorTotal = valorTotal + resultado;
+        let totalValorParcial = document.getElementById('018').value * 103.52;
+        let totalPessoasParcial = document.getElementById('018').value;
+
+        totalPessoas = totalPessoas + totalPessoasParcial;
+        totalValor = totalValor + totalValorParcial;
     }
     if(document.getElementById('1923').value > 0){
-        let resultado = (document.getElementById('1923').value * 136.20).toFixed(2);
-        document.getElementById('resultado1923').innerHTML = '19-23: \t' + document.getElementById('1923').value + '\tR$' + resultado;
-        totalPessoas = totalPessoas + parseFloat(document.getElementById('1923').value);
-        valorTotal = valorTotal + resultado;
-    }
+        let totalValorParcial = document.getElementById('1923').value * 136.20;
+        let totalPessoasParcial = document.getElementById('1923').value;
 
-    if(totalPessoas > 0 && valorTotal > 0){
-        document.getElementById('resultadoQuantidadeDePessoas').innerHTML = 'Total de pessoas: \t R$' + totalPessoas;
-        document.getElementById('resultadoValorFinal').innerHTML = 'Valor total: \tR$' + valorTotal;
+        totalPessoas = parseInt(totalPessoas) + parseInt(totalPessoasParcial);
+        totalValor = totalValor + totalValorParcial;
+    }
+    if(document.getElementById('2428').value > 0){
+        let totalValorParcial = document.getElementById('2428').value * 155.20;
+        let totalPessoasParcial = document.getElementById('2428').value;
+
+        totalPessoas = parseInt(totalPessoas) + parseInt(totalPessoasParcial);
+        totalValor = totalValor + totalValorParcial;
+    }
+    
+
+
+    
+
+
+    if(totalPessoas > 0 && totalValor > 0){
+        document.getElementById('resultadoQuantidadeDePessoas').innerHTML = 'Numero Total de Pessoas: \t' + totalPessoas;
+        if(totalPessoas > 1){
+            document.getElementById('resultadoValorFinal').innerHTML = 'Valor Total do Plano:\t R$ \t' + parseFloat(0.95 * totalValor).toFixed(2);
+        }else{
+            document.getElementById('resultadoValorFinal').innerHTML = 'Valor Total do Plano:\t R$ \t' + parseFloat(totalValor).toFixed(2);
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-    if(document.getElementById('1923').value > 0){
-        document.getElementById('resultado1923').innerHTML = '19-23: \t' + document.getElementById('1923').value + '\tR$' + (document.getElementById('1923').value * 136.20).toFixed(2);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('1923').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('1923').value * 136.20));
-    }
-    if(document.getElementById('2428').value > 0) {
-        document.getElementById('resultado2428').innerHTML = '24-28: \t' + document.getElementById('2428').value + '\tR$' + (document.getElementById('2428').value * 155.20);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('2428').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('2428').value * 155.20));
-    }
-    if(document.getElementById('2933').value > 0) {
-        document.getElementById('resultado2933').innerHTML = '29-33: \t' + document.getElementById('2933').value + '\tR$' + (document.getElementById('2933').value * 173.08);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('2933').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('2933').value * 173.08));
-    }
-    if(document.getElementById('3438').value > 0) {
-        document.getElementById('resultado3438').innerHTML = '34-38: \t' + document.getElementById('3438').value + '\tR$' + (document.getElementById('3438').value * 182.13);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('3438').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('3438').value * 182.13));
-    }
-    if(document.getElementById('3943').value > 0) {
-        document.getElementById('resultado3943').innerHTML = '39-43: \t' + document.getElementById('3943').value + '\tR$' + (document.getElementById('3943').value * 204.42);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('3943').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('3943').value * 204.42));
-    }
-    if(document.getElementById('4448').value > 0) {
-        document.getElementById('resultado4448').innerHTML = '44-48: \t' + document.getElementById('4448').value + '\tR$' + (document.getElementById('4448').value * 289.89);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('4448').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('4448').value * 289.89));
-    }
-    if(document.getElementById('4953').value > 0) {
-        document.getElementById('resultado4953').innerHTML = '49-53: \t' + document.getElementById('4953').value + '\tR$' + (document.getElementById('4953').value * 346.43);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('4953').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('4953').value * 346.43));
-    }
-    if(document.getElementById('5458').value > 0) {
-        document.getElementById('resultado5458').innerHTML = '54-58: \t' + document.getElementById('5458').value + '\tR$' + (document.getElementById('5458').value * 466.84);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('5458').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('5458').value * 466.84));
-    }
-    if(document.getElementById('59+').value > 0) {
-        document.getElementById('resultado59+').innerHTML = '59 + : \t' + document.getElementById('59+').value + '\tR$' + (document.getElementById('59+').value * 606.21);
-        totalPessoas = totalPessoas + parseInt(document.getElementById('59+').value);
-        valorTotal = valorTotal + parseInt((document.getElementById('59+').value * 606.21));
-    }
-*/
+
+<p id="resultado2933"></p>
+
+<p id="resultado3438"></p>
+
+<p id="resultado3943"></p>
+
+<p id="resultado4448"></p>
+
+<p id="resultado4953"></p>
+
+<p id="resultado5458"></p>
+
+<p id="resultado59+"></p> */
