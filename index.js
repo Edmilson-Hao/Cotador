bb8b6a601240c7fe9b85b871d78fc039df8586b7
Edmilson-Hao <edmilsonwp@gmail.com>
2020-11-29 11:46:29 +0000

    alteraçoes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //função para cotar o plano
 function cotar(){
     document.getElementById('resultado').style.display = 'block';
+    document.getElementById('voltarbtn').style.display = 'block';
     document.getElementById('corporcotador').style.display = 'none';
-
+    document.getElementById('header').style.display = 'none';
     //Verificando plano selecionado
     if(document.getElementById('selecionarplanos').value === 'HapvidaAmbulatorial'){
         cotarhapvidaambulatorial();
@@ -11,8 +12,7 @@ function cotar(){
         
 }
 
-//Ao terminar consulta da cotação, clicar no botão voltarbtn recarrega tela anterior
-function hideresultadodiv(){
+hideresultadodiv = () => {
     document.getElementById('resultado').style.display = 'none';
-    document.getElementById('corporcotador').style.display = 'block';
+    document.getElementById('voltarbtn').style.display = 'none';
 }
