c1afe98c344be0c7dbafc352b7fd2412fcdd6361
Edmilson-Hao <edmilsonwp@gmail.com>
2020-11-29 22:19:07 +0000

    Update Div Resultado

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ cotarHapvidaAmbulatorial = () => {
     var totalValor = 0;
     document.getElementById('subDivResultado').innerHTML = '';
 
+    //Tratamento da faixa etária 0 - 18
     if(document.getElementById('018').value > 0){
         let totalValorParcial = document.getElementById('018').value * 103.52;
         let totalPessoasParcial = document.getElementById('018').value;
@@ -14,8 +15,29 @@ cotarHapvidaAmbulatorial = () => {
             <table>
                 <tr>
                     <th>0 - 18</th>
-                    <th>` + totalPessoasParcial + `</th>
+                    <th>\t` + totalPessoasParcial + `</th>
+                    <th>R$103.52</th>
                     <th>` + totalValorParcial + `</th>
+                </tr>
+            </table>
+        `;
+    }
+
+    //Tratamento da faixa etária 19 - 23
+    if(document.getElementById('1923').value > 0){
+        let totalValorParcial = document.getElementById('1923').value * 136.20;
+        let totalPessoasParcial = document.getElementById('1923').value;
+
+        totalPessoas = totalPessoas + totalPessoasParcial;
+        totalValor = totalValor + totalValorParcial;
+
+        document.getElementById('subDivResultado').innerHTML += `
+            <table>
+                <tr>
+                    <th>19-23</th>
+                    <th>\t` + totalPessoasParcial + `</th>
+                    <th>R$136.20</th>
+                    <th>` + totalValorParcial.toFixed(2) + `</th>
                 </tr>
             </table>
         `;
@@ -23,12 +45,40 @@ cotarHapvidaAmbulatorial = () => {
 
     
 
-
+    //Imprimindo parte final do cotador
     if(totalPessoas > 0 && totalValor > 0){
-        document.getElementById('subDivResultado').innerHTML += `
-            <span class='spanResultado'>Total Pessoas: \t ` + totalPessoas + `</span>
+
+        document.getElementById('subDivResultado').insertAdjacentHTML("afterbegin", `
+            <table>    
+                <tr>
+                    <th>Faixa Etária</th>
+                    <th>Quantidade</th>
+                    <th>Unitário</th>
+                    <th>Valor</th>
+                </tr>
+            </table>
             <br>
-            <span class='spanResultado'>Valor Total: \t R$` + totalValor + `</span>
+        `);
+
+        document.getElementById('subDivResultado').innerHTML += `
+            <br>
+            <p class='paragraphSpanResultado'>
+                <span class='textoSpanResultado'>Total Pessoas:</span>
+                <span class='valorSpanResultado'>` + parseInt(totalPessoas) + `</span>
+            </p>
+            
+            <br>
+
+            <hr>
+
+            <p class='paragraphSpanResultado'>
+                <span class='textoSpanResultado'>Valor Total:</span>
+                <span class='valorSpanResultado'>R$:` + parseFloat(totalValor).toFixed(2) + `</span>
+            </p>
+
+            <br>
+
+            <hr>
         `;
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 //função para cotar o plano
 cotar = () => {
+    var planoSelecionado = document.getElementById('selecionarplanos').value;
     document.getElementById('corpoCotador').style.display = 'none';        
     document.getElementById('resultado').style.display = 'block';
 
-    if(document.getElementById('selecionarplanos').value === 'HapvidaAmbulatorial') cotarHapvidaAmbulatorial();
+    switch(planoSelecionado){
+        case 'hapvidaAmbulatorial':
+            cotarHapvidaAmbulatorial();
+        break;
+        case 'Mangoes':
+        case 'Papayas':
+            console.log('Mangoes and papayas are $2.79 a pound.');
+            // expected output: "Mangoes and papayas are $2.79 a pound."
+            break;
+        default:
+            console.log(`Sorry, we are out of ${expr}.`);
+    }
 }
 
 voltar = () => {
